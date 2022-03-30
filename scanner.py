b460c56a1221e22a74f401cccfacad67ec166a36
f20190014 <f20190014@goa.bits-pilani.ac.in>
2022-03-30 15:52:15 +0530

    scannerstart

---
+++ scanner.py
@@ -0,0 +1,16 @@
+from sly import Lexer
+
+class CalcLexer(Lexer):
+    # Set of token names.   This is always required
+    tokens = { KEYWORD, NUMBER, MOVE, ASSIGNOP,BOOLEAN, DELIMITER}
+
+    # String containing ignored characters between tokens
+    ignore = ' \t'
+
+    # Regular expression rules for tokens
+    KEYWORD = r'[CONFIG][ROWS][COLUMNS][MUSIC][SPEED][NEXTQ][BLOCKTYPE1][BLOCKTYPE2][BLOCKTYPE3][BLOCKTYPE4][BLOCKTYPE5][BLOCKTYPE6][BLOCKTYPE7][TIMER][TIMEDGAME]'
+    NUMBER  = r'\d+'
+    ASSIGNOP  = r'='
+    BOOLEAN = r'[TRUE][FALSE]'
+    MOVE = r'[RIGHT][LEFT][SOFTDROP][HARDDROP][ROTATERIGHT][ROTATELEFT]'
+    DELIMITER = r'\n'
