0848752d11721283041bc32a31fde7ebff042811
WhiskeyLake <amansahore23@gmail.com>
2022-03-31 14:23:34 +0530

    Scanner testing

--- scanner.py
+++ scanner.py
@@ -11,6 +11,12 @@ class CalcLexer(Lexer):
     KEYWORD = r'[CONFIG][ROWS][COLUMNS][MUSIC][SPEED][NEXTQ][BLOCKTYPE1][BLOCKTYPE2][BLOCKTYPE3][BLOCKTYPE4][BLOCKTYPE5][BLOCKTYPE6][BLOCKTYPE7][TIMER][TIMEDGAME]'
     NUMBER  = r'\d+'
     ASSIGNOP  = r'='
-    BOOLEAN = r'[TRUE][FALSE]'
+    BOOLEAN = r'[TRUE] | [FALSE]'
     MOVE = r'[RIGHT][LEFT][SOFTDROP][HARDDROP][ROTATERIGHT][ROTATELEFT]'
     DELIMITER = r'\n'
+
+if __name__ == '__main__':
+    data = 'TRUE'
+    lexer = CalcLexer()
+    for tok in lexer.tokenize(data):
+        print('type=%r, value=%r' % (tok.type, tok.value))
