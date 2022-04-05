a87b98cf904a077052cbdbfcd88b60dd6a944c29
Aman Sharma <amansahore23@gmail.com>
2022-04-05 15:45:40 +0530

    git config test

--- driver.py
+++ driver.py
@@ -4,16 +4,19 @@ from tetrisparser import TetrisParser
 
 def main():
     lexer = TetrisLexer()
-    while True:
-        program = open('program.rpj','r')
-        text = input(">>> ")
-        tokens = lexer.tokenize(text) # Creates a generator of tokens
-        for t in tokens:
-            print(t.type)
-            print(t.value)
-        # parser = TetrisParser()
-        # parser.parse(tokens) # The entry point to the parser
-        # print(parser.last_item_on_stack)
+    parser = TetrisParser()
+    program = open('test.rpj','r')
+    text = program.readlines()
+    print(len(text))
+    for string in text:
+        tokens = lexer.tokenize(string) # Creates a generator of tokens
+        for tok in tokens:
+            # print(tok)
+            print('type=%r, value=%r' % (tok.type, tok.value))
+        parser.parse(tokens)    
+    
+    # parser.parse(tokens) # The entry point to the parser
+    # print(parser.last_item_on_stack)
 
 if __name__ == '__main__':
     main()
--- tetrisparser.py
+++ tetrisparser.py
@@ -5,6 +5,10 @@ class TetrisParser(Parser):
     
     tokens = TetrisLexer.tokens
 
+    def __init__(self):
+        self.names = { }
+
     @_('KEYWORD ASSIGNOP NUMBER')
     def statement(self, p):
         self.names[p.KEYWORD] = p.NUMBER
+        print(self.names)
